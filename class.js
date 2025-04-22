ef0f75072042018c42427048dbcfc25acc70ebe5
souravrajbhar1234567 <saurabhrajbhar223225@gmail.com>
2025-04-22 14:52:27 +0530

    saurabh

---
+++ class.js
@@ -0,0 +1,71 @@
+/*class Person {
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+
+        // Create a new instance of Person with both firstname and lastname
+        let p = new Person('Maike', 'Smith'); // Provide both arguments
+        console.log("hi", p.firstname, p.lastname);
+    }
+}
+
+// Example of creating a Person instance
+let person = new Person('John', 'Doe');*/
+// methods -> we dont use the function keywords
+/*class Person {
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    greet() {
+        console.log("hi there", this.firstname);
+    }
+}
+
+// Create a new instance of Person with both firstname and lastname
+let p = new Person('Maike', 'Smith'); // Provide both arguments
+p.greet(); // Call the greet method*/
+/*class Person {
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    greet() {
+        console.log("hi there", this.firstname);
+    }
+
+    compliment(name, object) {
+        return "that is wonderful " + object + ", " + name; // Corrected string concatenation
+    }
+}
+
+// Create an instance of Person
+let p = new Person('Maike', 'Smith');
+
+// Call the compliment method
+let compliment = p.compliment("Harry", "hat");
+console.log(compliment);*/
+/*class person{
+    #firstname;
+    #lastname;
+    constructor(firstname, lastname){
+        this.#firstname=firstname;
+        this.#lastname=lastname;
+        let p= new person("maria a","saga")
+
+    }
+}*/
+// getter and setter
+/* person = {
+    firstName: "John",
+    lastName: "Doe",
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };*/
+  //let p = new Person ("maria", "saga")
+  //console.log(p.firstname);
+  
+  
